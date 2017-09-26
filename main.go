1b69498438a5d05005046cab7d4e60ab17e3d888
MaBing <cumt_ttr@163.com>
2017-09-26 00:39:38 -0700

    日志打印行数

--- main.go
+++ main.go
@@ -1,14 +1,48 @@
 package main
 
 import (
-	log "github.com/sirupsen/logrus"
+	"github.com/sirupsen/logrus"
 	"github.com/urfave/cli"
 	"os"
+	"path"
+	"runtime"
+	"strings"
 )
 
 const usage = `mydocker is a simple container runtime implementation.
 			   The purpose of this project is to learn how docker works and how to write a docker by ourselves
 			   Enjoy it, just for fun.`
+
+type ContextHook struct{}
+
+func (hook ContextHook) Levels() []logrus.Level {
+	//return logrus.AllLevels
+	return []logrus.Level{logrus.PanicLevel,
+						  logrus.FatalLevel,
+						  logrus.ErrorLevel,
+						  logrus.WarnLevel,
+						  logrus.InfoLevel,
+						  logrus.DebugLevel,
+						  }
+}
+
+func (hook ContextHook) Fire(entry *logrus.Entry) error {
+	pc := make([]uintptr, 3, 3)
+	cnt := runtime.Callers(6, pc)
+
+	for i := 0; i < cnt; i++ {
+		fu := runtime.FuncForPC(pc[i] - 1)
+		name := fu.Name()
+		if !strings.Contains(name, "github.com/sirupsen/logrus") {
+			file, line := fu.FileLine(pc[i] - 1)
+			entry.Data["file"] = path.Base(file)
+			entry.Data["func"] = path.Base(name)
+			entry.Data["line"] = line
+			break
+		}
+	}
+	return nil
+}
 
 func main() {
 	app := cli.NewApp()
@@ -22,13 +56,14 @@ func main() {
 
 	app.Before = func(context *cli.Context) error {
 		// Log as JSON instead of the default ASCII formatter.
-		log.SetFormatter(&log.JSONFormatter{})
+		logrus.AddHook(ContextHook{})
+		logrus.SetFormatter(&logrus.JSONFormatter{})
 
-		log.SetOutput(os.Stdout)
+		logrus.SetOutput(os.Stdout)
 		return nil
 	}
 
 	if err := app.Run(os.Args); err != nil {
-		log.Fatal(err)
+		logrus.Fatal(err)
 	}
 }
